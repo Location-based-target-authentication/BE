99b205f00ea6b6c6b7a2a20c223ac38ad425a59e
HAJIHYUK <gkwlgurdydydy@naver.com>
2025-02-17 07:50:40 +0900

    초기커밋

---
+++ Location-based-target-authentication/src/main/java/com/swyp/entity/DayOfWeek.java
@@ -0,0 +1,5 @@
+package com.swyp.entity;
+
+public enum DayOfWeek {
+    MON, TUE, WED, THU, FRI, SAT, SUN
+}
---
+++ Location-based-target-authentication/src/test/java/com/swyp/LocationBasedTargetAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationBasedTargetAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

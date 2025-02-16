7caae8c52380263296a91bca0f60dc5199563d99
HAJIHYUK <168079884+HAJIHYUK@users.noreply.github.com>
2025-02-17 07:47:37 +0900

    Delete Location-based-target-authentication directory

--- Location-based-target-authentication/src/main/java/com/swyp/Swagger/dto/user/AutoLoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.swyp.Swagger.dto.user;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "자동 로그인 설정 요청")
-public record AutoLoginRequest(
-    @Schema(description = "자동 로그인 사용 여부", example = "true")
-    Boolean enabled
-) {} 

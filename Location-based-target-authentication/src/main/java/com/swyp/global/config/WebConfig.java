2d664cf42a31cc505549f1ca84ea53f7aa0333d3
kjunh972 <kjunh972@gmail.com>
2025-03-03 23:06:09 +0900

    ssl 설정 변경

--- Location-based-target-authentication/src/main/java/com/swyp/global/config/WebConfig.java
+++ Location-based-target-authentication/src/main/java/com/swyp/global/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.swyp.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -33,5 +35,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
     }
 }

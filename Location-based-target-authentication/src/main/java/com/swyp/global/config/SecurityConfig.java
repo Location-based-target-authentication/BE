5acc7d912abd487adfd8a99c664df9eefeff94b2
kjunh972 <kjunh972@gmail.com>
2025-03-04 02:14:07 +0900

    fix: 포트번호 수정

--- Location-based-target-authentication/src/main/java/com/swyp/global/config/SecurityConfig.java
+++ Location-based-target-authentication/src/main/java/com/swyp/global/config/SecurityConfig.java
@@ -21,7 +21,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+                .requiresChannel(channel -> channel
+                        .anyRequest().requiresSecure())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**").permitAll()
                 );

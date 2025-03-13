c240a5e1bf8f13e0652e445c5c1c985dc3222e10
kjunh972 <kjunh972@gmail.com>
2025-03-13 23:53:32 +0900

    feat: Liquibase 설정 추가

--- Location-based-target-authentication/src/main/java/com/swyp/global/config/SwaggerConfig.java
+++ Location-based-target-authentication/src/main/java/com/swyp/global/config/SwaggerConfig.java
@@ -31,15 +31,30 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .description("위치 기반 목표 인증 서비스 API 문서")
                 .version("1.0.0");
 
-        SecurityScheme securityScheme = new SecurityScheme()
-                .name("JWT")
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .name("Bearer")
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
                 .bearerFormat("JWT");
 
+        SecurityScheme oauth2Scheme = new SecurityScheme()
+                .name("OAuth2")
+                .type(SecurityScheme.Type.OAUTH2)
+                .flows(new io.swagger.v3.oas.models.security.OAuthFlows()
+                        .authorizationCode(new io.swagger.v3.oas.models.security.OAuthFlow()
+                                .authorizationUrl("https://kauth.kakao.com/oauth/authorize")
+                                .tokenUrl("https://kauth.kakao.com/oauth/token")
+                                .scopes(new io.swagger.v3.oas.models.security.Scopes()
+                                        .addString("profile_nickname", "프로필 닉네임")
+                                        .addString("profile_image", "프로필 이미지")
+                                        .addString("account_email", "계정 이메일"))));
+
         return new OpenAPI()
                 .info(info)
-                .addSecurityItem(new SecurityRequirement().addList("JWT"))
-                .components(new Components().addSecuritySchemes("JWT", securityScheme));
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .addSecurityItem(new SecurityRequirement().addList("OAuth2"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", bearerScheme)
+                        .addSecuritySchemes("OAuth2", oauth2Scheme));
     }
 }
